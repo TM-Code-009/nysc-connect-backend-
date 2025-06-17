aa929355f087d4d38132d2a68af25c53b1ca1170
TM-Code-009 <osakweterrynduka@gmail.com>
2025-06-17 13:02:03 +0100

    session add for oauth

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
 import cors from "cors";
 import connectDB from "./utils/db";
-import authRoutes from "./routes/authRoutes";
 import passport from "passport";
-import session from "express-session";
 import "./config/passport";
+import session from "express-session";
 
 dotenv.config();
 const app = express();
@@ -16,6 +16,16 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/auth", authRoutes);
+app.use(session({
+  secret: process.env.SESSION_SECRET || "your_fallback_secret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === "production", // true on Render
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+  },
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
