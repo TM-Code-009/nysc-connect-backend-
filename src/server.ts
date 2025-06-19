1c52f4218781110fd552bf2006a073ca73de7ec0
TM-Code-009 <osakweterrynduka@gmail.com>
2025-06-19 08:59:31 +0100

    cors fixed

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import MongoStore from "connect-mongo";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 
