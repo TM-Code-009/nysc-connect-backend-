3d76d1b5e40117c9990b86f7ee37ee722c714561
TM-Code-009 <osakweterrynduka@gmail.com>
2025-07-04 10:24:53 +0100

    user get route created

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -25,3 +25,21 @@ export const updateProfile = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+export const getProfile = async (req: Request, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const userId = (req.user as IUser)._id;
+
+    const user = await User.findById(userId).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Get Profile Error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- src/routes/useRoutes.ts
+++ src/routes/useRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { updateProfile } from "../controllers/userController";
+import { updateProfile, getProfile } from "../controllers/userController";
 import { protect } from "../middlewares/authMiddleware";
 import { asyncHandler } from "../utils/asynuHandler";
 
 const router = express.Router();
 
+// 🔐 Use protect middleware for authenticated routes
+router.get("/profile", asyncHandler(getProfile));
 router.put("/profile", asyncHandler(updateProfile));
 
 export default router;
